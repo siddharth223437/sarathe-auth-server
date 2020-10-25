3a11ff1bfef6a8dddb7d32038c2a2697349eb408
siddharth223437 <siddharthsarathe.nmims@gmail.com>
2020-10-25 16:03:20 -0500

    Initial commit

---
+++ src/main/java/com/sarathe/auth/server/SaratheAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.sarathe.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaratheAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaratheAuthServerApplication.class, args);
+	}
+
+}
